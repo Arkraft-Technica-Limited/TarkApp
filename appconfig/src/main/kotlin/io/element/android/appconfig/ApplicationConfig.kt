dd6ddf899e503331f5eddf52e4eab2820a12e999
Derrick Andera (DK) <derickandera70@gmail.com>
2025-11-30 22:56:05 +0300

    User end Text update.

--- appconfig/src/main/kotlin/io/element/android/appconfig/ApplicationConfig.kt
+++ appconfig/src/main/kotlin/io/element/android/appconfig/ApplicationConfig.kt
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2025 Element Creations Ltd.
- * Copyright 2023-2025 New Vector Ltd.
+ * Copyright (c) 2025 Tark Coms Ltd.
+ * Copyright 2025 Arkraft Technica  Ltd.
  *
- * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial.
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Tark-Commercial.
  * Please see LICENSE files in the repository root for full details.
  */
 
@@ -12,24 +12,24 @@ object ApplicationConfig {
     /**
      * Application name used in the UI for string. If empty, the value is taken from the resources `R.string.app_name`.
      * Note that this value is not used for the launcher icon.
-     * For Element, the value is empty, and so read from `R.string.app_name`, which depends on the build variant:
-     * - "Element X" for release builds;
-     * - "Element X dbg" for debug builds;
-     * - "Element X nightly" for nightly builds.
+     * For Tark, the value is empty, and so read from `R.string.app_name`, which depends on the build variant:
+     * - "Tark" for release builds;
+     * - "Tark dbg" for debug builds;
+     * - "Tark nightly" for nightly builds.
      */
     const val APPLICATION_NAME: String = ""
 
     /**
-     * Used in the strings to reference the Element client.
+     * Used in the strings to reference the Tark client.
      * Cannot be empty.
-     * For Element, the value is "Element".
+     * For Tark, the value is "Tark".
      */
-    const val PRODUCTION_APPLICATION_NAME: String = "Element"
+    const val PRODUCTION_APPLICATION_NAME: String = "Tark"
 
     /**
-     * Used in the strings to reference the Element Desktop client, for instance Element Web.
+     * Used in the strings to reference the Tark Desktop client, for instance Tark Web.
      * Cannot be empty.
-     * For Element, the value is "Element". We use the same name for desktop and mobile for now.
+     * ForTark, the value is "Tark". We use the same name for desktop and mobile for now.
      */
-    const val DESKTOP_APPLICATION_NAME: String = "Element"
+    const val DESKTOP_APPLICATION_NAME: String = "Tark"
 }
--- plugins/src/main/kotlin/config/BuildTimeConfig.kt
+++ plugins/src/main/kotlin/config/BuildTimeConfig.kt
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2025 Element Creations Ltd.
- * Copyright 2025 New Vector Ltd.
+ * Copyright (c) 2025 Tark Coms Ltd.
+ * Copyright 2025 Arkraft Technica  Ltd.
  *
- * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial.
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Tark-Commercial.
  * Please see LICENSE files in the repository root for full details.
  */
 
@@ -10,7 +10,7 @@ package config
 
 object BuildTimeConfig {
     const val APPLICATION_ID = "io.element.android.x"
-    const val APPLICATION_NAME = "Element X"
+    const val APPLICATION_NAME = "Tark"
     const val GOOGLE_APP_ID_RELEASE = "1:912726360885:android:d097de99a4c23d2700427c"
     const val GOOGLE_APP_ID_DEBUG = "1:912726360885:android:def0a4e454042e9b00427c"
     const val GOOGLE_APP_ID_NIGHTLY = "1:912726360885:android:e17435e0beb0303000427c"
