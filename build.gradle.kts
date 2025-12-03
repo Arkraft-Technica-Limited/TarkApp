a2ebfffd401c9877228ec43508ecf2459e98f6f5
Derrick Andera (DK) <derickandera70@gmail.com>
2025-12-03 06:22:15 +0300

    Tark firbase plugin install

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ plugins {
     alias(libs.plugins.ktlint)
     alias(libs.plugins.dependencygraph)
     alias(libs.plugins.sonarqube)
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
 
 tasks.register<Delete>("clean").configure {
